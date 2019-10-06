9df9c1df7487cfa563bef126b57e02030f053ebe
Marketa <markettka@email.cz>
2019-10-06 23:37:16 +0200

    add map, filter operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -20,27 +21,31 @@ export class HomeComponent implements OnInit, OnDestroy {
     // });
 
     // build custom observable to replace interval
-    let customInterval = Observable.create( observer => {
+    const customInterval = Observable.create( observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if(count == 2) {
+        if (count === 2) {
           observer.complete();
         }
-        if(count > 3) {
+        if (count > 3) {
           observer.error(new Error('Count is greater than 3!'));
-          
         }
-        count ++
-      }, 1000)
-    })
+        count ++;
+      }, 1000);
+    });
+
 
     // subscribe to custom observable
-   this.firstObsSubscription = customInterval.subscribe(data => {
-     console.log(data)},
-    error => {
-      alert(error.message)
-    }, () => console.log('Completed'))
+    this.firstObsSubscription =
+      customInterval.pipe(filter( data => {
+        return data > 0; }),
+        map((data: number) => {
+        return 'Round ' + (data+1);
+      }))
+      .subscribe(data => {console.log(data); },
+      error => { alert(error.message);},
+      () => console.log('Completed'));
   }
 
   ngOnDestroy(): void {
